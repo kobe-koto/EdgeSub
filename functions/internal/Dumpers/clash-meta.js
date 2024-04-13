a0ddf1b3509930c48a1586521df5ba5984e9f401
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2024-04-13 22:10:25 +0800

    [Add Dumper Support] [clash-meta] Hysteria2

--- functions/internal/Dumpers/clash-meta.js
+++ functions/internal/Dumpers/clash-meta.js
@@ -11,4 +11,20 @@ export default class Dumper {
 
         return true;
     }
+    hysteria2 (hy2Object) {
+        return {
+            type: hy2Object.__Type,
+            name: hy2Object.__Remark,
+            server: hy2Object.Hostname,
+            port: hy2Object.Port,
+            password: hy2Object.Auth,
+            auth: hy2Object.Auth,
+            obfs: hy2Object.Query.obfs,
+            "obfs-password": hy2Object.Query["obfs-password"],
+            "client-fingerprint": this.config.ClientFingerprint,
+            "skip-cert-verify": this.config.SkipCertVerify,
+            udp: this.config.UDP,
+        }
+    }
+    hy2 = this.hysteria2;
 }
