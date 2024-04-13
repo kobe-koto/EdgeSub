6e1480ccce2e4ae7ab276e775937dc83e692c734
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2024-04-13 21:50:35 +0800

    [Add Parser Support] Hysteria1 & Hysteria2

--- functions/internal/NodeParser.js
+++ functions/internal/NodeParser.js
@@ -1,3 +1,35 @@
 export default class NodeParser {
     constructor () {}
+    hysteria (HYURL) {
+        const URLObject = new URL (HYURL);
+
+        const HY = {
+            __Type: "hysteria1",
+            __Remark: decodeURIComponent(URLObject.hash.replace(/^#/, "")),
+            Hostname: URLObject.hostname,
+            Port: URLObject.port,
+            Query: {}
+        }
+        for (const [key, value] of URLObject.searchParams) {
+            HY.Query[key] = value
+        }
+        return HY;
+    }
+    hysteria2 (HY2URL) {
+        const URLObject = new URL (HY2URL);
+
+        const HY2 = {
+            __Type: "hysteria2",
+            __Remark: decodeURIComponent(URLObject.hash.replace(/^#/, "")),
+            Auth: URLObject.username,
+            Hostname: URLObject.hostname,
+            Port: URLObject.port,
+            Query: {}
+        }
+        for (const [key, value] of URLObject.searchParams) {
+            HY2.Query[key] = value
+        }
+        return HY2;
+    }
+    hy2 = this.hysteria2;
 }
