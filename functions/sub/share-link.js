af9f3750de5922dffd60e0f3796e62cc14b95833
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2024-06-08 17:14:37 +0800

    logic improvements for share link endpoint

--- functions/sub/share-link.js
+++ functions/sub/share-link.js
@@ -1,20 +1,25 @@
 import getParsedSubData from "../internal/getParsedSubData.js";
-
 import { ShareLinkDumper } from "../internal/Dumpers/share-link.js";
+
 
 
 export async function onRequest (context) {
     const { request } = context;
+
+    // do convert
+    const Proxies = await getParsedSubData(new URL(request.url).searchParams.get("url"), request.headers);
     let Dumper = new ShareLinkDumper();
-    const ParsedSubData = await getParsedSubData(new URL(request.url).searchParams.get("url"), request.headers);
     let ShareLinkArray = [];
-    for (let i of ParsedSubData) {
+    for (let i of Proxies) {
         if (Dumper[i.__Type]) {
             ShareLinkArray.push(Dumper[i.__Type](i))
         }
     }
+    
+    // generate final response
+    let ShareLinkResponse = ShareLinkArray.join("\n");
 
-    return new Response(ShareLinkArray.join("\n"), {
+    return new Response(ShareLinkResponse, {
         status: 200,
         headers: {
             "Content-Type": "text/plain, charset=utf-8",
