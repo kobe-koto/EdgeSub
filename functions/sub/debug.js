75886de9568f44625401216564630e54f8b152bb
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2024-07-18 14:01:15 +0800

    fix /debug

--- functions/sub/debug.js
+++ functions/sub/debug.js
@@ -3,8 +3,8 @@ import getParsedSubData from "../internal/getParsedSubData.js";
 export async function onRequest (context) {
     const { request } = context;
     const ParsedSubData = await getParsedSubData(new URL(request.url).searchParams.get("url"), request.headers);
-
-    return new Response(JSON.stringify(ParsedSubData.data), {
+    
+    return new Response(JSON.stringify(ParsedSubData), {
         status: 200,
         headers: {
             "Content-Type": "application/json, charset=utf-8",
