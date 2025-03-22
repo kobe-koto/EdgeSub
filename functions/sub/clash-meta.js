08be23675cc30d9f38f40abf81a8d239b2cfff2b
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2025-03-22 21:47:20 +0800

    will check ss cipher for ClAsH oRiGiNaL

--- functions/sub/clash-meta.js
+++ functions/sub/clash-meta.js
@@ -16,11 +16,24 @@ export async function onRequest (context, isClashOriginal = false) {
     // process proxies if its a clash original request.
     if (isClashOriginal === true) {
         const ClashSupportedProxyType = ["trojan", "vmess", "ss", "ssr", "http", "socks5" /* "snell" */];
+        const ClashSupportedSSCipher = ["aes-128-gcm", "aes-192-gcm", "aes-256-gcm", "chacha20-ietf-poly1305", "xchacha20-ietf-poly1305", "aes-128-cfb", "aes-192-cfb", "aes-256-cfb", "rc4-md5", "chacha20-ietf", "xchacha20", "aes-128-ctr", "aes-192-ctr", "aes-256-ctr"]
 
         console.info(`[Main] It's a Clash Original Config request!, \nfiltering out these: ${ClashSupportedProxyType.join(", ")}`)
 
         // filter the proxies
-        Proxies = Proxies.filter( i => ClashSupportedProxyType.includes(i.__Type) )
+        Proxies = Proxies
+            .filter( i => ClashSupportedProxyType.includes(i.__Type) )
+            .filter( i => {
+                console.log(i)
+                if (i.__Type === "ss") {
+                    console.log(i.Auth.cipher)
+                    if (ClashSupportedSSCipher.includes(i.Auth.cipher)) {
+                        return true;
+                    }
+                } else {
+                    return true;
+                }
+            })
     }
 
     // a javascript object !!! not YAML !!!
