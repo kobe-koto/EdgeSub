31f83680b1b18ab8614d5b06b61751d5ee9beff7
792121621 <56538747+792121621@users.noreply.github.com>
2025-05-07 02:48:03 +0800

    Delete functions directory

--- functions/sub/base64.js
+++
@@ -1,5 +0,0 @@
-import { onRequest as ShareLinkOnRequest } from "./share-link.js";
-
-export async function onRequest (context) {
-    return await ShareLinkOnRequest(context, true)
-}
--- functions/sub/clash.js
+++
@@ -1,5 +0,0 @@
-import { onRequest as ClashMetaOnRequest } from "./clash-meta.js";
-
-export async function onRequest (context) {
-    return await ClashMetaOnRequest(context, true)
-}
